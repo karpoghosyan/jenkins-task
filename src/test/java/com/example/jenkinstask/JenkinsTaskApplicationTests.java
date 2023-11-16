9dd03e786d27cff946feebe6536fff56fd299849
Karen Poghosyan <karpoghosyan99@gmail.com>
2023-11-17 02:53:08 +0400

    initial commit

---
+++ src/test/java/com/example/jenkinstask/JenkinsTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
